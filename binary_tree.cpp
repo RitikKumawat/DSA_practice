889cfdf7e9914f03cabd723cf8531264fb2fac7e
RitikKumawat <rtkkmwt@gmail.com>
2023-06-09 18:32:05 +0530

    changes

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class node{
@@ -55,23 +56,50 @@ void postorder(node* root){
     cout<< root->data;
 }
 
+void level_order_traversal(node* root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        node* temp =q.front();
+        
+        q.pop();
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left!=NULL)
+                q.push(temp->left);
+            if(temp->right!=NULL)
+                q.push(temp->right);
+            
+        }
+    }
+}
 
 int main(){
 node* root=new node(1);
-root->left =new node(2);
-root->right =new node(3);
-root->left->left =new node(4);
-root->left->right =new node(5);
-root->right->left =new node(6);
-root->right->right =new node(7);
+root->left=new node(2);
+root->left->left=new node(4);
+root->left->right=new node(5);
+root->right=new node(3);
+root->right->left=new node(6);
+root->right->right=new node(7);
+// root=buildTree(root);
 
+// 1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1 
 preorder(root);
 cout<<endl;
 inorder(root);
 cout<<endl;
 postorder(root);
 cout<<endl;
-
+level_order_traversal(root);
 
 
 return 0;
